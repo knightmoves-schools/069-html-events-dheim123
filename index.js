8bd697b8fcd0dbeefe35b48084872141c3d945ed
dheim123 <dheim@midland.k12.ia.us>
2025-12-05 18:11:18 +0000

    1

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function calculateSquare(){
     let value = document.getElementById('number').value;
     let result = document.getElementById('result');
 
+
     //add code here
-    
+    if (isNaN(value) || value === '') {
+        result.innerHTML = 'Invalid, please enter a number';
+    }else {
+        result.innerHTML = value * value;
+    };
+
 }
+
+document.getElementById('calculate').addEventListener('click', calculateSquare);
